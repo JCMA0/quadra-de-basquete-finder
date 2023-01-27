85dabc5dbb546f9420315e4c507ee3a0b089171b
JCMA <88733511+JCMA0@users.noreply.github.com>
2023-01-26 22:32:05 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+const carousel = document.querySelector("[data-target='carousel']");
+const card = carousel.querySelector("[data-target='card']");
+const leftButton = document.querySelector("[data-action='slideLeft']");
+const rightButton = document.querySelector("[data-action='slideRight']");
+const carouselWidth = carousel.offsetWidth;
+const cardStyle = card.currentStyle || window.getComputedStyle(card)
+const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
+const cardCount = carousel.querySelectorAll("[data-target='card']").length;
+let offset = 0;
+const maxX = -((cardCount / 3) * carouselWidth + 
+               (cardMarginRight * (cardCount / 3)) - 
+               carouselWidth - cardMarginRight);
+
+
+// Add the click events
+leftButton.addEventListener("click", function() {
+  if (offset !== 0) {
+    offset += carouselWidth + cardMarginRight;
+    carousel.style.transform = `translateX(${offset}px)`;
+    }
+})
+  
+rightButton.addEventListener("click", function() {
+  if (offset !== maxX) {
+    offset -= carouselWidth + cardMarginRight;
+    carousel.style.transform = `translateX(${offset}px)`;
+  }
+})
